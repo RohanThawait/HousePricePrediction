1e7e69e972cc05b4bf675b918c93dd7bdd630c6c
RohanThawait <thawaitbusiness@gmail.com>
2025-02-01 13:07:34 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+to create pyhton environment: python -m venv env_name
+to activate environemnt : .\env_name\Scripts\activate
+to install requirements.txt : pip install -r requirements.txt
+"""
+print("Hello World")
